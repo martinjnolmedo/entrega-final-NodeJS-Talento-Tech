17ebee0fd153dc7a2ae0f499b76b2563ec1206ff
martinjnolmedo <olmedo.molmedo@gmail.com>
2025-07-24 21:15:13 -0300

    instalado Firebase

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,5 +1,16 @@
-export const getAllProducts = (req, res) => {
-    return [
-        { id: 1, name: "Product 1", price: 100 }
-    ];
+import { db } from './firebase.js';
+import { collection, getDocs } from 'firebase/firestore';
+const productsCollection = collection(db, 'products');
+
+export const getAllProducts = async () => {
+    try {
+        const snapshot = await getDocs(productsCollection);
+        const products = snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data()
+        }));
+        return products;
+    } catch (error) {
+        console.error("Error al recibir productos:", error);
+    };
 };
